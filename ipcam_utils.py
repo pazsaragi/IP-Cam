854ce3557213a533b60c9986aa2a563fcc6c1f32
ps <38565348+paruliansaragi@users.noreply.github.com>
2019-08-14 08:27:16 +0100

    Update ipcam_utils.py

--- ipcam_utils.py
+++ ipcam_utils.py
@@ -214,7 +214,7 @@ class Camera(object):
     def startStream(self):
         """
         start stream via rtsp
-        rtsp://<user>:<pw>@10.32.100.175:554/11
+        rtsp://<user>:<pw>@<ip>:<port>/11
         """
         self.video
         cv2.imshow('object detection', cv2.resize(self.imshowIMG, (800,600)))
